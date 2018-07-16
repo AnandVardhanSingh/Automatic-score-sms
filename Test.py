7fadc132dec57775374e38163a3f5762a4e32005
anandvsingh <anand_vardhan_singh@outlook.com>
2018-07-16 14:29:42 +0530

    changes in test.py, now handles worksheet to form text messages

--- Test.py
+++ Test.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from openpyxl import load_workbook
 from tkinter import filedialog
 
 
@@ -6,6 +7,11 @@ root = tk.Tk()
 root.withdraw()
 
 file_path = filedialog.askopenfilename()
-workbook = xlrd.open_workbook(file_path)
-
-sheet = workbook.open_by_index(0)
+wb = load_workbook (file_path)
+date = input('Enter the date of the exam: ')
+subject = input ('Enter the subject/topic of the exam: ')
+ws = wb.active
+for row in ws:
+	if str(row[0].value).lower().strip() != 'name':
+		message = 'Your ward ' + str(row[0].value) +' has scored ' +str(row[2].value) + ' on the test of '+ subject  +' held on '+ date
+		print(message)
